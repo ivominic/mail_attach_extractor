9941747fb374ae00f4f57c02bcb635a184c902c1
Ivo Minic <ivo.minic@gmail.com>
2022-05-23 12:42:48 +0200

    Prepared files for move

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
 const pdfUtil = require("pdf-to-text");
 const fs = require("fs");
 
-const pdf_path = "d:/test/Forma Ziirat Banka.pdf";
-/*pdfUtil.info(pdf_path, function (err, info) {
-  if (err) throw err;
-  console.log(info);
-});*/
-
 const readAllFiles = (directoryPath) => {
   fs.readdir(directoryPath, function (err, filenames) {
     if (err) {
-      onError(err);
+      console.error(err);
       return;
     }
     filenames.forEach(function (filename) {
-      fs.readFile(directoryPath + filename, "utf-8", function (err, content1) {
-        if (err) {
-          console.error(filename, err);
-          return;
-        }
-        if (filename.endsWith(".pdf")) {
-          pdfUtil.pdfToText(directoryPath + filename, function (err, content) {
+      let currentPath = directoryPath + filename;
+      if (filename.endsWith(".pdf")) {
+        pdfUtil.pdfToText(currentPath, function (err, content) {
+          if (err) throw err;
+          checkFileContent(filename, content);
+          let newPath = directoryPath + "new/";
+          fs.mkdir(newPath, { recursive: true }, (err) => {
             if (err) throw err;
-            checkFileContent(filename, content);
           });
-        }
-      });
+          fs.rename(currentPath, newPath + filename, function (err) {
+            if (err) throw err;
+            console.log("Successfully moved!", newPath + filename);
+          });
+        });
+      }
     });
   });
 };
