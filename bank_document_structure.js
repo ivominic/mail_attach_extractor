fa92c96aae3c237349ba91ef810638f5586b2809
Ivo Minic <ivo.minic@gmail.com>
2022-06-12 16:41:07 +0200

    On desktop machine throws error permission on renaming file

--- bank_document_structure.js
+++ bank_document_structure.js
@@ -2,15 +2,25 @@ const util = require("./util");
 
 function findAccountNumberInSpecificLine(accountNumber, content) {
   let contentArray = util.contentToArrayByLine(content);
-
-
-
+  let filename = "";
+  !filename && (filename = domesticAddiko(accountNumber, contentArray));
+  console.log("filename", filename);
+  return filename;
 }
 
 function domesticAddiko(accountNumber, contentArray) {
-  let retVal = ""
+  let retVal = "";
   let location = contentArray[9];
-  console.log("Tekst za pretragu:", location)
-
-
+  console.log("Tekst za pretragu:", location);
+  console.log("acc_no", accountNumber);
+  console.log("tekst", location.substring(73, 90));
+  if (accountNumber === location.substring(73, 90)) {
+    console.log("NAŠAO!!!!!!!");
+    retVal = accountNumber + ".pdf";
+  }
+  return retVal;
 }
+
+module.exports = {
+  findAccountNumberInSpecificLine,
+};
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const pdfUtil = require("pdf-to-text");
 const fs = require("fs");
 const util = require("./util");
+const bankStructure = require("./bank_document_structure");
 
 let folderData = JSON.parse(fs.readFileSync("data/json/folders.json", { encoding: "utf8" }));
 
@@ -44,41 +45,43 @@ function fileContent(filename, content) {
   let companyAccountNumber = "";
   let multipleCompanyNames = "";
   let multipleCompanyAccounts = "";
+  let destFilename = "";
 
   companiesData.forEach((element) => {
     if (element.ziro_racun && element.ziro_racun !== "null") {
       //TODO: Ovdje pozvati provjere za svaku od banaka i vratiti putanju u koju se prebacuje fajl.
-      if (content.indexOf(element.ziro_racun) !== -1) {
+      if (content.indexOf(element.ziro_racun) !== -1 && !destFilename) {
         numberOfMatches++;
         companyName = element.sinonim;
         companyFolder = element.folder;
         companyAccountNumber = element.ziro_racun;
         multipleCompanyNames += companyName + ", ";
         multipleCompanyAccounts += companyAccountNumber + ", ";
+        destFilename = bankStructure.findAccountNumberInSpecificLine(element.ziro_racun, content);
       }
     }
   });
 
-  if (numberOfMatches !== 1) {
-    if (numberOfMatches === 0) {
-      let logMessage = `Nije nađen nijedan žiro račun za fajl: "${filename}"`;
-      util.writeLog(logMessage, true);
-    } else {
-      let logMessage = `Višestruki za: "${filename}", kompanije: "${multipleCompanyNames}", žr: "${multipleCompanyAccounts}"`;
-      util.writeLog(logMessage, true);
-    }
-  } else {
-    let logMessage = `Uspjeh: "${filename}", kompanija: "${companyName}", žiro račun: "${companyAccountNumber}"`;
-    let destinationDir = `${writeDirectoryPath}/${companyFolder}/`;
-    moveFiles(filename, readDirectoryPath, destinationDir);
+  if (destFilename) {
+    let destinationFile = `${writeDirectoryPath}/${companyFolder}/${destFilename}`;
+    let logMessage = `Uspjeh: "${filename}", kompanija: "${companyName}", žiro račun: "${companyAccountNumber}", putanja: "${destinationFile}"`;
+    moveFiles(readDirectoryPath + filename, destinationFile);
     util.writeLog(logMessage, false);
+  } else {
+    let logMessage = `Nije nađen nijedan žiro račun za fajl: "${filename}"`;
+    util.writeLog(logMessage, true);
   }
 }
 
-function moveFiles(filename, sourceDir, destinationDir) {
+function moveFiles(sourceDir, destinationDir) {
   if (util.makeDir(destinationDir)) {
-    fs.rename(sourceDir + filename, destinationDir + filename, function (err) {
+    fs.rename(sourceDir, destinationDir, function (err) {
       if (err) util.processError(err);
     });
   }
 }
+
+/*moveFiles(
+  "C:/test/input/Forma Addiko Banka.pdf",
+  "C:/test/output/IZVODI Z.R. ELEKTRONSKA ARHIVA 021/027 BP Proing/2022/Domaci/Addiko/555.pdf"
+);*/
