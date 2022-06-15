49985d06d4785ec9051a5a95050b383277ab67fb
Ivo Minic <ivo.minic@gmail.com>
2022-06-15 19:58:39 +0200

    Completed Erste html parse

--- html_parse.js
+++ html_parse.js
@@ -4,21 +4,46 @@ const util = require("./util");
 
 function findAccountNumberInHtml(accountNumber, content) {
   let filename = "";
-
-  console.log("accountNumber", accountNumber);
   const $ = cheerio.load(content, null, false);
 
   !filename && (filename = domesticErste(accountNumber, $));
+  !filename && (filename = foreignErste(accountNumber, $));
 
   return filename;
 }
 
-//TODO: Branko da odredi koji account čitamo
 function domesticErste(accountNumber, $) {
   let retVal = "";
   let dateText = $("p").next("p").text();
   let dateValue = dateText.substring(dateText.length - 12);
-  let accountValue = $("table").next("table").next("table").find("tr").next().find("td").next().html();
+  let accountValue1 = $("table").next("table").next("table").find("tr").find("td").next().html();
+  let accountValue2 = $("table").next("table").next("table").find("tr").next().find("td").next().html();
+  let numberValue = $("table")
+    .next("table")
+    .next("table")
+    .find("tr")
+    .next()
+    .next()
+    .next()
+    .find("td")
+    .next()
+    .html()
+    .substring(0, 3);
+
+  if (accountNumber === accountValue1 || accountNumber === accountValue2) {
+    retVal = `br.${numberValue} od ${dateValue}.htm`;
+  }
+  return retVal;
+}
+
+function foreignErste(accountNumber, $) {
+  let retVal = "";
+  let dateText = $("p").next("p").text();
+  let dateValue = dateText.substring(dateText.length - 12);
+  let testIban = $("table").next("table").next("table").find("tr").find("td").html();
+  if (testIban.trim() !== "IBAN:") return retVal;
+  let accountValue1 = $("table").next("table").next("table").find("tr").find("td").next().html();
+  let accountValue2 = $("table").next("table").next("table").find("tr").next().find("td").next().html();
   let numberValue = $("table")
     .next("table")
     .next("table")
@@ -32,8 +57,8 @@ function domesticErste(accountNumber, $) {
     .html()
     .substring(0, 3);
 
-  if (accountNumber === accountValue) {
-    retVal = `br.${numberValue} od ${dateValue}.pdf`;
+  if (accountNumber === accountValue1 || accountNumber === accountValue2) {
+    retVal = `br.${numberValue} od ${dateValue}.htm`;
   }
   return retVal;
 }
